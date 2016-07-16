c67466aab42327de582e9b9fe961c19820731c48
chakwong87 <chakwong87@gmail.com>
2016-07-15 21:23:35 -0400

    modified venmo clean and median

--- src/venmo_clean.py
+++ src/venmo_clean.py
@@ -1,1 +1,15 @@
+# Program that cleans venmo text file
+import simplejson as sj
+import sys
+import os
+import urllib
+txt = urllib.urlopen(
+    "https://raw.githubusercontent.com/InsightDataScience/coding-challenge/master/data-gen/venmo-trans.txt").read()
 
+input_file = open(sys.argv[1])
+
+for line in input_file:
+    try: # extraction of timestamp, tweet text from json. If a unicode tweet is found triggers exception
+        json = sj.loads(line.strip())
+
+input_file = open(sys.argv[1])
--- src/venmo_mediandegree.py
+++ src/venmo_mediandegree.py
@@ -1,1 +1,17 @@
+import sys
+import json
+import time
+import itertools
+import datetime
+import simplejson as sj
+import sys
+import os
+import urllib
+txt = urllib.urlopen(
+    "https://raw.githubusercontent.com/InsightDataScience/coding-challenge/master/data-gen/venmo-trans.txt").read()
 
+input_file = o
+
+for line in input_file:
+    try: # extraction of timestamp, tweet text from json. If a unicode tweet is found triggers exception
+        json = sj.loads(line.strip())
